c9d32c7166e4c8031bd7c9f313d9088b846c638c
cryptnotiq <43958441+cryptnotiq@users.noreply.github.com>
2020-06-02 00:25:24 +0200

    first commit

---
+++ testmodule/keeper/params.go
@@ -0,0 +1,22 @@
+package keeper
+/*
+// TODO: Define if your module needs Parameters, if not this can be deleted
+
+import (
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cryptnotiq/testapp/x/testmodule/types"
+)
+
+// GetParams returns the total set of testmodule parameters.
+func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
+	k.paramspace.GetParamSet(ctx, &params)
+	return params
+}
+
+// SetParams sets the testmodule parameters to the param space.
+func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	k.paramspace.SetParamSet(ctx, &params)
+}
+*/
